8efb0206a5ea0af1363aad57db06cffde80d3a17
leese-lee <leese.yeon.lee@gmail.com>
2025-04-20 18:35:10 +0900

    feat: main entry point for Flask app

---
+++ main.py
@@ -0,0 +1,11 @@
+print(1)
+from app.utils.knn import knn_from_input_sentence, knn_result_to_html
+print(2)
+from app.data_loader import data_list, model
+print(3)
+from app import create_app
+
+app = create_app()
+if __name__ == "__main__":
+    app.run(debug=True)
+
